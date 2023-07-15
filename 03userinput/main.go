7a8e7ab5c0256e7a166b4718d512c9095c19d5fe
Khushbu2411 <khushbu.agrawal@sendinblue.com>
2023-07-15 17:00:28 +0530

    Basics of go

--- 03userinput/main.go
+++ 03userinput/main.go
@@ -4,6 +4,8 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"strconv"
+	"strings"
 )
 
 func main() {
@@ -17,4 +19,13 @@ func main() {
 	input, _ := reader.ReadString('\n')
 	fmt.Println("Thanks for rating, ", input)
 	fmt.Printf("Type of this rating is %T", input)
+
+	numrating, err := strconv.ParseFloat(strings.TrimSpace(input), 64)
+
+	if ( err != nil){
+		fmt.Println(err)
+	} else {
+		fmt.Println("Added 1 to your rating: ",numrating + 1)
+	}
+
 }
