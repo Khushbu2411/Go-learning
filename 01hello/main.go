a16c7e5e4069e319f9707f2f56e36d7fa734ba1b
Khushbu2411 <khushbu.agrawal@sendinblue.com>
2023-07-15 15:25:16 +0530

    Basics of go

---
+++ 01hello/main.go
@@ -0,0 +1,10 @@
+package main
+
+import "fmt"
+
+//function declaration
+func main() {
+
+	//print statement
+	fmt.Println("Hello from Khushbu....")
+}
---
+++ 02variables/main.go
@@ -0,0 +1,50 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+
+	//type string
+	var username string = "Khushbu"
+	fmt.Println("Variables - username: ", username)
+	fmt.Printf("username is of type: %T \n", username)
+
+	//type bool
+	var isLoggedIn bool = true
+	fmt.Println("Variables - isLoggedIn: ", isLoggedIn)
+	fmt.Printf("isLoggedIn is of type: %T \n", isLoggedIn)
+
+	//type int
+	var smallVal int = 956435645788766789
+	fmt.Println("Variables - smallVal: ", smallVal)
+	fmt.Printf("smallVal is of type: %T \n", smallVal)
+
+
+	//type float32
+	var smallFloat float32 = 255.3453645467
+	fmt.Println("Variables - smallFloat: ", smallFloat)
+	fmt.Printf("smallFloat is of type: %T \n", smallFloat)
+
+	//type float64
+	var bigFloat float64 = 255.3453645467
+	fmt.Println("Variables - bigFloat: ", bigFloat)
+	fmt.Printf("bigFloat is of type: %T \n", bigFloat)
+
+	// declaring type
+	var anotherVariable string
+	fmt.Println("Variables - anotherVariable: ", anotherVariable)
+	fmt.Printf("anotherVariable is of type: %T \n", anotherVariable)
+
+	//implicit type
+	var website = "learncode.fr" 
+	fmt.Println("Variables - website: ", website)
+	fmt.Printf("website is of type: %T \n", website)
+
+	// no var style - using short variable declaration
+	numberOfUsers := 30045.00
+	fmt.Println("Variables - numberOfUsers: ", numberOfUsers)
+	fmt.Printf("numberOfUsers is of type: %T \n", numberOfUsers)
+
+}
